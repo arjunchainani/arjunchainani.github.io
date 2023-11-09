3895c2e8a6219faa30edba56e880f8fa036fb08c
arjunchainani <arjun.k.chainani@gmail.com>
2023-11-09 11:39:17 -0500

    working on projects section

---
+++ src/components/Projects.tsx
@@ -0,0 +1,19 @@
+const ProjectNames = ["AirVision", "SegNet", "QuantumSpotify", "VidNote", "Quantum Superdense Coding Simulator"];
+
+const ProjectsList = {
+  "AirVision": "Description here",
+  "SegNet": "Description here",
+  "QuantumSpotify": "Description here",
+  "VidNote": "Description here",
+  "Quantum Superdense Coding Simulator": "Description here",
+};
+
+const Projects = () => {
+  return (
+    <div className="skills_title">
+      <h2>Projects</h2>
+    </div>
+  )
+};
+
+export default Projects;
