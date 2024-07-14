8a26b57ead79908d982dc80f71333c7bcc115fc7
ArjunChainani <arjun.k.chainani@gmail.com>
2024-07-14 14:21:55 -0400

    fixed fonts issue and added experience section

---
+++ src/components/ItemList.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+interface Props {
+  ItemNames: string[];
+  ItemArray: string[][];
+  type: string;
+}
+
+const ItemList = ({ ItemNames, ItemArray, type }: Props) => {
+  const [selectedItemIndex, changeSelectedItemIndex] = useState(-1);
+
+  return (
+    <div className="projects_container">
+      {ItemNames.map((item, index) => {
+        return (
+          <div
+            className={
+              selectedItemIndex == index ? "project selected" : "project"
+            }
+            onClick={(event) => {
+              event.preventDefault();
+              selectedItemIndex == index
+                ? changeSelectedItemIndex(-1)
+                : changeSelectedItemIndex(index);
+            }}
+          >
+            <h4 className="project_text">{item}</h4>
+            {selectedItemIndex == index ? (
+              <p className="project_description">
+                {type == "projects" ? (
+                  <p>
+                    Github Link:
+                    <a
+                      href={ItemArray[selectedItemIndex][0]}
+                      className="git_link"
+                      onClick={(event) => {
+                        event.preventDefault();
+                      }}
+                    >
+                      {ItemArray[selectedItemIndex][0]}
+                    </a>
+                  </p>
+                ) : (
+                  <></>
+                )}
+                {ItemArray[selectedItemIndex][1]}
+              </p>
+            ) : (
+              <></>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ItemList;
